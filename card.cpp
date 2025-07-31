a9e30e3fd344ca5ec309ab7c56044f64df0ea997
Kenji Fong <kjfkenji@gmail.com>
2025-07-31 18:20:46 -0400

    Added the card class and header file. Decide later if both classes are needed are just the header file for the deck class

---
+++ card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kjfke on 7/31/2025.
+//
+
+#include "card.h"
---
+++ card.h
@@ -0,0 +1,16 @@
+//
+// Created by kjfke on 7/31/2025.
+//
+
+#ifndef CARD_H
+#define CARD_H
+
+
+
+class card {
+
+};
+
+
+
+#endif //CARD_H
